cf2cfed47107433fe524516e709b6a42c3748af5
Francisco Arce <fjarce.g@gmail.com>
2023-10-24 18:43:10 -0300

    Add index route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 const app = require('./app');
+const PORT = 3000;
 
-async function init() {
-    await app.listen(3000);
-    console.log('Server started on port 3000');
-    console.log('API doc in /api-docs');
+// async function init() {
+//     await app.listen(3000);
+//     console.log('Server started on port 3000');
+//     console.log('API doc in /api-docs');
 
-}
+// }
 
-init();
+// init();
 
+app.listen(PORT, () => {
+    console.log(`
+        API listening on PORT ${PORT}
+        Docs in /api-docs
+    `);
+});
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳');
+});
