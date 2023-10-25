be6dce2c92be97f00e2ab7e50185a927729d1a78
Francisco Arce <fjarce.g@gmail.com>
2023-10-25 00:46:48 -0300

    add vercel conf

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,8 +4,9 @@ require('dotenv').config()
 module.exports = {
    secret: 'my_secret',
    db_port: process.env.PORT,
-   db_user: process.env.DB_USER || 'postgres',
-   db_pass: process.env.DB_PASSWORD || 'Hellen2012',
-   db_database: process.env.DB_DATABASE || 'sushi_store',
-   db_host: process.env.DB_HOST || 'localhost'
+   db_user: process.env.DB_USER,
+   db_pass: process.env.DB_PASSWORD,
+   db_database: process.env.DB_DATABASE,
+   db_host: process.env.DB_HOST,
+   db_endpoint: process.env.ENDPOINT_ID
 }
