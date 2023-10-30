a23a4c3b63ba4800cc30b6c8a050f54739ad2109
Francisco Arce <fjarce.g@gmail.com>
2023-10-29 21:28:39 -0300

    other changes

--- frontend/src/config/clientAxios.jsx
+++ frontend/src/config/clientAxios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'http://192.168.1.112:3000/api/sushi-store',
-    // baseURL: 'http://localhost:3000/api/sushi-store'
+    // baseURL: 'http://192.168.1.112:3000/api/sushi-store',
+    baseURL: 'http://localhost:3000/api/sushi-store',
 })
 
 export default clientAxios;
